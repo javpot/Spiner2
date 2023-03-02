5f4dc9cc944ab00122ef8ad0a0f3d02408975049
GajdaRichard <richard.gajda0@gmail.com>
2023-03-01 21:11:02 -0500

    saAWDdca

--- app/src/main/java/com/example/spiner2/MainActivity.java
+++ app/src/main/java/com/example/spiner2/MainActivity.java
@@ -2,13 +2,38 @@ package com.example.spiner2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import org.xml.sax.SAXException;
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
 
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.xml.parsers.ParserConfigurationException;
+
+public class MainActivity extends AppCompatActivity {
+    private String[] strArray;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        System.out.println("|AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+        try {
+            @SuppressLint("ResourceType") InputStream is = getResources().openRawResource(R.xml.cities);
+            XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+            XmlPullParser parser = factory.newPullParser();
+            parser.setInput(is, null);
+
+            System.out.println("zazazazazazazazazazazazazaza");
+
+        } catch (Exception e) {
+        e.printStackTrace();
+        System.out.println("fwqedwq :");
+        System.out.println("------\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+    }
     }
 }
