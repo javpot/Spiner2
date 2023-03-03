20fa6b889397f49f3cd1095e1e8dc40211b47025
GajdaRichard <richard.gajda0@gmail.com>
2023-03-03 10:39:09 -0500

    saAWDdcaaadawd

--- app/src/main/java/com/example/spiner2/MainActivity.java
+++ app/src/main/java/com/example/spiner2/MainActivity.java
@@ -11,6 +11,11 @@ import android.content.res.Resources;
 import android.content.res.XmlResourceParser;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
@@ -31,6 +36,7 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -38,25 +44,58 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
 public class MainActivity extends AppCompatActivity {
-    private ArrayList<String> strArray = new ArrayList<>();
+    private final HashMap<String, ArrayList<String>> liste = new HashMap<String, ArrayList<String>>();
     List<String[]> csvData;
+    ArrayAdapter<String> adapter2;
+    ArrayAdapter<String> adapter;
+    ArrayList<String> keys = new ArrayList<>();
+    Spinner s1 = (Spinner) findViewById(R.id.spinner);
+    Spinner s2 = (Spinner) findViewById(R.id.spinner2);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("|AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+
         try {
             InputStream inputStream = this.getResources().openRawResource(R.raw.cities);
             CSVReader csvReader = new CSVReader();
             List<String[]> csvData = csvReader.readCSV(inputStream);
 
             for(String[] s : csvData) {
-                System.out.println(s[0] + "\t" + s[1]);
+                //System.out.println(s[0] + "\t" + s[1]);
+                if (liste.get(s[1]) == null) {
+                    liste.put(s[1], new ArrayList<String>());
+                }
+                liste.get(s[1]).add(s[0]);
             }
+
+            keys.add("null");
+            for(String s : liste.keySet()) {
+                //System.out.println(s + "\t" + liste.get(s).toString());
+                keys.add(s);
+            }
+
+            adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, keys);
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            s1.setAdapter(adapter);
+
+            s1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                @Override
+                public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                    String key = parent.getItemAtPosition(position).toString();
+                    adapter2 = new ArrayAdapter<String>(s2.getContext(), android.R.layout.simple_spinner_item, liste.get(key));
+                    adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                    s2.setAdapter(adapter2);
+                }
+
+                @Override
+                public void onNothingSelected(AdapterView <?> parent) {
+                }
+            });
+
         } catch (Exception e) {
+            System.out.println(e.toString());
             e.printStackTrace();
-            System.out.println("fwqedwq :");
-            System.out.println("------\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
         }
     }
 }
